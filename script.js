ec8e04e81580f7dbee873513d2fcb7969050c14c
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-03-11 16:52:16 +0100

    esecizio1 snack

--- script.js
+++ script.js
@@ -7,13 +7,64 @@
 
 
 const getPostTitle = id => {
-    const promise = new Promise ((resolev, reject)=>{
+    const promise = new Promise((resolev, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
-        .then(response=> response.json())
-        .then(obj=> resolev(obj))
-        .catch(reject)
+            .then(response => response.json())
+            .then(obj => resolev(obj))
+            .catch(reject)
     })
     return promise
 }
 
-getPostTitle(1).then(obj=> console.log(obj.title)).catch(error=>console.error(error))
+getPostTitle(1).then(obj => console.log(obj)).catch(error => console.error(error))
+
+
+// const getPost = id => {
+//     const post = new Promise((resolve, reject) => {
+//         fetch(`https://dummyjson.com/posts/${id}`)
+//             .then(resolve => resolve.json())
+//             .then(obj => resolve(obj))
+//             .catch(reject)
+//     });
+//     const user = new Promise ((resolve, reject)=>{
+//         fetch(`https://dummyjson.com/users/${post.userId}`)
+//         .then(resolve=>resolve.json())
+//         .then(obj => resolve(obj))
+//         .catch(reject)
+//     })
+
+//     const userPost = {
+//         User: user,
+//         Post : post,
+//     }
+//     return userPost
+
+// } 
+
+
+
+
+
+const getPost = (id) => {
+    return fetch(`https://dummyjson.com/posts/${id}`)
+        .then(res => res.json())
+        .then(post => {
+            return fetch(`https://dummyjson.com/users/${post.userId}`)
+                .then(res => res.json())
+                .then(user => {
+                    return {
+                        user: user,
+                        Post: post,
+                    }
+                })
+        })
+
+}
+
+getPost(1).then(obj => console.log(obj)).catch(error => console.error(error))
+
+
+
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
